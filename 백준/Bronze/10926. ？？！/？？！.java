c3b3fbecf840e6b10bd4a3e59afa3e211d79cec8
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2022-07-11 03:17:53 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		String x = input.nextLine();
+				
+		System.out.println(x + "??!");
+	
+
+	}
+
+}
