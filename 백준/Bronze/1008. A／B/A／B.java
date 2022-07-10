5a932c0a2e8eb14bad1453d2666d7b6e322e7044
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2022-07-11 03:13:12 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		double x = input.nextDouble();
+		double y = input.nextDouble();
+		
+		System.out.println(x/y);
+
+	}
+
+}
