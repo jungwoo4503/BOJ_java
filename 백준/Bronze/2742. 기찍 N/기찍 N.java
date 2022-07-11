ea8c679af23d038da21a5a0dcad1ff9c9fe1cd34
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2022-07-12 05:58:16 +0900

    [Bronze III] Title: 기찍 N, Time: 1080 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		int i;
+		
+		for(i=n; i>0; i--) {
+			
+			System.out.println(i);
+		}
+		
+	}
+}
