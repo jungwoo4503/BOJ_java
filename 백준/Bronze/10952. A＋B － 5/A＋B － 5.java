854170d1c2474d0041b021fc7254b8c2d07a533b
jungwoo4503 <108951535+jungwoo4503@users.noreply.github.com>
2022-07-12 07:19:46 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17904 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(true) {
+			int a = input.nextInt();
+			int b = input.nextInt();
+			
+			if(a==0 && b==0) {
+				break;
+			}
+			
+			System.out.println(a+b);
+		}
+	
+	}
+}
